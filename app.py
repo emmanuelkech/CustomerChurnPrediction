6dba6e9074f8562ccaef57f87a27678dfb6c904e
Emmanuel Ikechukwu <88853963+emmanuelkech@users.noreply.github.com>
2024-12-24 17:30:31 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -24,4 +24,4 @@ def predict():
 if __name__ == '__main__':
     # Use the PORT environment variable if available, otherwise default to 5000
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True, port=port)
